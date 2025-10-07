0bf5da5199d31d806aefc64d725189391a3fcfa1
smallproject <j.a.villas@live.com.ph>
2025-10-07 22:23:20 +0200

    added resume preview temp design

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,32 @@ import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Header from "./components/Header.jsx";
+import ResumeForm from "./components/ResumeForm.jsx";
+import ResumePreview from "./components/ResumePreview.jsx";
 
 function App() {
-
+    const [formData, setFormData] = useState({
+        fullName: "",
+        email: "",
+        phone: "",
+        summary: "",
+        experience: "",
+        education: "",
+        skills: "",
+    });
   return (
-      <div className="container py-5">
+      <div className="d-flex flex-column min-vh-100">
           <Header/>
+          <main className="container my-5">
+              <div className="row">
+                  <div className="col-md-6">
+                      <ResumeForm formData={formData} setFormData={setFormData}/>
+                  </div>
+                  <div className="col-md-6">
+                      <ResumePreview data={formData}/>
+                  </div>
+              </div>
+          </main>
       </div>
   )
 }
